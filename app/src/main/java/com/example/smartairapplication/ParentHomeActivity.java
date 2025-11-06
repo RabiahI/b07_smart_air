82e0689ea22fa71258bf3d7aa1fc689fde6e5ce5
Jason Villablanca <villabj001@gmail.com>
2025-11-06 15:16:14 -0500

    feat: connected ChildManagement to ParentHome through a button

--- app/src/main/java/com/example/smartairapplication/ParentHomeActivity.java
+++ app/src/main/java/com/example/smartairapplication/ParentHomeActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,16 +12,18 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class ParentHomeActivity extends AppCompatActivity {
 
-    Button button;
+    Button logoutButton;
+    Button childButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parent_home);
 
-        button = findViewById(R.id.logout);
+        logoutButton = findViewById(R.id.logout);
+        childButton = findViewById(R.id.btn_child);
 
-        button.setOnClickListener(new View.OnClickListener(){
+        logoutButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 FirebaseAuth.getInstance().signOut();
@@ -29,6 +32,15 @@ public class ParentHomeActivity extends AppCompatActivity {
                 finish();
             }
 
+        });
+
+        childButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ChildManagementActivity.class);
+                startActivity(intent);
+                finish();
+            }
         });
     }
 }
