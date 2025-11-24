d53da99c7499013184c3d6d37b707066d6747c93
Rabiah Islam <islamrabiah5@gmail.com>
2025-11-23 19:51:37 -0500

    feat: implemented UI and backend for step-by-step technique guide

--- app/src/main/java/com/example/smartairapplication/TechniqueIntro.java
+++ app/src/main/java/com/example/smartairapplication/TechniqueIntro.java
@@ -12,9 +12,6 @@ import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class TechniqueIntro extends AppCompatActivity {
 
