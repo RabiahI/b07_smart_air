70704904b89f74565b74525d199fa8dd7d524c1d
villabj001 <villabj001@gmail.com>
2025-11-27 22:54:27 -0500

    feat: added childID to change it so that its shows depending on which child is being shown

--- app/src/main/java/com/example/smartairapplication/Alert.java
+++ app/src/main/java/com/example/smartairapplication/Alert.java
@@ -5,16 +5,18 @@ public class Alert {
     private String message;
     private long timestamp;
     private String severity;
+    private String childId;
 
     public Alert() {
         // Default constructor required for calls to DataSnapshot.getValue(Alert.class)
     }
 
-    public Alert(String type, String message, long timestamp, String severity) {
+    public Alert(String type, String message, long timestamp, String severity, String childId) {
         this.type = type;
         this.message = message;
         this.timestamp = timestamp;
         this.severity = severity;
+        this.childId = childId;
     }
 
     public String getType() {
@@ -47,5 +49,13 @@ public class Alert {
 
     public void setSeverity(String severity) {
         this.severity = severity;
+    }
+
+    public String getChildId() {
+        return childId;
+    }
+
+    public void setChildId(String childId) {
+        this.childId = childId;
     }
 }
