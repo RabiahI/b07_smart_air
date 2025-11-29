ebb1277f0d4fd478af817c372ee850c1462dc855
Rabiah Islam <islamrabiah5@gmail.com>
2025-11-28 22:07:12 -0500

    feat: added date picker for DOB and auto-calculate age

--- app/src/main/java/com/example/smartairapplication/UserRoleManager.java
+++ app/src/main/java/com/example/smartairapplication/UserRoleManager.java
@@ -7,7 +7,6 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -15,7 +14,6 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class UserRoleManager {
-
     public static void redirectUserBasedOnRole(Activity activity, String uid) {
         DatabaseReference userRef = FirebaseDatabase.getInstance().getReference("Users");
         String[] roles = {"Parent", "Provider"};
