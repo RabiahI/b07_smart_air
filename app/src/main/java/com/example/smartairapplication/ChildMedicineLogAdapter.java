a6533dd97e9269d2c15eda82ed4300487e5e8e02
Rabiah Islam <islamrabiah5@gmail.com>
2025-11-29 20:12:27 -0500

    feat: added functionality for seeing pef history on child homepage

--- app/src/main/java/com/example/smartairapplication/ChildMedicineLogAdapter.java
+++ app/src/main/java/com/example/smartairapplication/ChildMedicineLogAdapter.java
@@ -7,7 +7,6 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.text.SimpleDateFormat;
